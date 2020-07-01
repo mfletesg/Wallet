5bbe6a8e53fffb918208bb21a6ce7a8e82e729f3
Miguel.Fletes <mfletesg@gmail.com>
2020-06-30 22:53:14 -0500

    Creación de Componentes en React

---
+++ resources/js/components/TransferList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const TransferList = () => (
+    <table className="table">
+        <tbody>
+            <tr>
+                <td>Ejemplo</td>
+                <td>1200</td>
+            </tr>
+        </tbody>
+    </table>
+)
+
+export default TransferList
